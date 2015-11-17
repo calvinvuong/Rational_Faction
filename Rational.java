f758af713cf1f5ddfe66b6402e0a478d183e835f
Calvin Vuong <calvin@boxer.local>
2015-11-17 16:03:18 -0500

    More comments added on Rational

--- Rational.java
+++ Rational.java
@@ -8,6 +8,11 @@ public class Rational{
     private int numerator;
     private int denominator;
 
+    //main method; test cases
+    public static void main(String[] args){
+
+    }
+
     //default constructor, default 0/1
     public Rational(){
 	numerator = 0;
@@ -18,12 +23,14 @@ public class Rational{
     //if q == 0, set to 0/1
     public Rational(int p, int q){
 	this();
-	if (q == 0){ 
+	if (q == 0){ //denom should not be 0...
+	    //set rational # equal to 0
 	    numerator = 0;
 	    denominator = 1;
 	    System.out.println("Invalid denominator; cannot be 0. Rational Number set to 0/1"); //print divide by 0 error
 	}
-	else{
+	else{ //if params fine...
+	    //set numer and denomin as requested
 	    numerator = p;
 	    denominator = q;
 	}
@@ -34,9 +41,10 @@ public class Rational{
 	return numerator + "/" + denominator;
     }
 
+    //returns the value of number as a float 
     public double floatValue(){
-	double floatQuotient;
-	floatQuotient = (double)(numerator) / (double)(denominator);
-	return floatQuotient;
+	double retFloat;
+	retFloat = (double)(numerator) / (double)(denominator); //typecast to prevent int division
+	return retFloat;
     }
 }
